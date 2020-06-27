fa0b785c1b4b5d6d6da689b6e4be005849226ab5
Alex Seufert <amseufert@gmail.com>
2020-06-27 12:35:20 -0400

    init commit

---
+++ API_Consumption/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using API_Consumption.Models;
+
+namespace API_Consumption.Controllers
+{
+    public class HomeController : Controller
+    {
+        string BASE_URL = "https://rss.itunes.apple.com/api/v1/us/ios-apps/top-free/all/100/explicit.json";
+        string itunesData = "";
+
+        public ActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            //Parks parks = null;
+            Models.OuterJSON feed = null;
+
+            client.BaseAddress = new Uri(BASE_URL);
+
+            try
+            {
+                HttpResponseMessage response = client.GetAsync(BASE_URL).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    itunesData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!itunesData.Equals(""))
+                {
+                    // JsonConvert is part of the NewtonSoft.Json Nuget package
+                    feed = JsonConvert.DeserializeObject<OuterJSON>(itunesData);
+                }
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View(feed);
+        }
+    }
+}
---
+++ API_Consumption/Models/Apps.cs
@@ -0,0 +1,47 @@
+﻿namespace API_Consumption.Models
+{
+    public class OuterJSON
+    {
+        public Feed feed { get; set; }
+    }
+
+    public class Feed
+    {
+        public Author author { get; set; }
+        public string copyright { get; set; }
+        public string country { get; set; }
+        public string icon { get; set; }
+        public string id { get; set; }
+        public string title { get; set; }
+        public string updated { get; set; }
+        public Results[] results { get; set; }
+    }
+
+    public class Results
+    {
+        public string artistId { get; set; }
+        public string artistName { get; set; }
+        public string artistUrl { get; set; }
+        public string artworkUrl100 { get; set; }
+        public string copyright { get; set; }
+        public Genres[] genres { get; set; }
+        public string id { get; set; }
+        public string kind { get; set; }
+        public string name { get; set; }
+        public string releaseDate { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Genres
+    {
+        public string genreId { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Author
+    {
+        public string name { get; set; }
+        public string uri { get; set; }
+    }
+}
